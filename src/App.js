06b48bce895808773988fb9b3c4720a9a017c32b
ayushtank2003 <ayushtaank2003@gmail.com>
2024-09-19 10:42:46 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>React card Component</h1>
+      </div>
+      <div className="card">
+        <card/>
+      </div>
     </div>
   );
-}
+};
+
+const card=()=>{
+  return(
+    <div className="card">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/330px-Image_created_with_a_mobile_phone.png"
+        alt="img"
+      />
+      <div className="card-body">
+        <h2>Title of the card</h2>
+        <p>caption text about the card </p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
